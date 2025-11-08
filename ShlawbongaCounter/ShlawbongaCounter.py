20f7fc070e2fe978149b398288a13be8978dd8d7
alexko-cloud <msg.alexko@gmail.com>
2025-11-08 13:21:20 -0800

    Cleaned up sub update logic and changed bits update logic

--- ShlawbongaCounter/ShlawbongaCounter.py
+++ ShlawbongaCounter/ShlawbongaCounter.py
@@ -4,28 +4,26 @@ class ShlawbongaCounter:
     def __init__(self):
         self.total_subs = 0
         self.last_sub_total = 0
-        self.subs_gained = 0
         self.total_bits = 0
-        self.last_bits_total = 0
-        self.bits_gained = 0
     
     def update_subs(self, new_sub_total):
-        if new_sub_total > self.last_sub_total:
-            self.subs_gained = new_sub_total - self.last_sub_total
-            self.total_subs += self.subs_gained
+        gained = new_sub_total - self.last_sub_total
+
+        if gained > 0:
+            self.total_subs += gained
         
         self.last_sub_total = new_sub_total
     
     def update_bits(self, bits):
-        if bits > self.last_bits_total:
-            self.bits_gained = bits - self.last_bits_total
-            self.total_bits += bits
-        
-        self.last_bits_total = bits
+        self.total_bits += bits
         
     @property
     def shlawbonga_count(self):
-        return self.subs_gained + (self.bits_gained // config.BITS_PER_HIT)
+        return self.total_subs + (self.total_bits // config.BITS_PER_HIT)
+
+    def init_txt_file(self, path):
+        with open(path, "w") as f:
+            f.write(f"SHLAWBONGA COUNT: 0")
 
     def write_count(self, path):
         with open(path, "w") as f:
