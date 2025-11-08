1c485979b57923940eae3e84785952269f35093d
alexko-cloud <msg.alexko@gmail.com>
2025-11-08 11:17:16 -0800

    Changed subs and bits calculation to reset to 0 each initialization

--- ShlawbongaCounter/ShlawbongaCounter.py
+++ ShlawbongaCounter/ShlawbongaCounter.py
@@ -4,21 +4,28 @@ class ShlawbongaCounter:
     def __init__(self):
         self.total_subs = 0
         self.last_sub_total = 0
+        self.subs_gained = 0
         self.total_bits = 0
+        self.last_bits_total = 0
+        self.bits_gained = 0
     
     def update_subs(self, new_sub_total):
         if new_sub_total > self.last_sub_total:
-            subs_delta = new_sub_total - self.last_sub_total
-            self.total_subs += subs_delta
+            self.subs_gained = new_sub_total - self.last_sub_total
+            self.total_subs += self.subs_gained
         
         self.last_sub_total = new_sub_total
     
     def update_bits(self, bits):
-        self.total_bits += bits
-    
+        if bits > self.last_bits_total:
+            self.bits_gained = bits - self.last_bits_total
+            self.total_bits += bits
+        
+        self.last_bits_total = bits
+        
     @property
     def shlawbonga_count(self):
-        return self.total_subs + (self.total_bits // config.BITS_PER_HIT)
+        return self.subs_gained + (self.bits_gained // config.BITS_PER_HIT)
 
     def write_count(self, path):
         with open(path, "w") as f:
